66d84836b66eec5e8cfbda2372e747e2eb778029
12 <keel-1@yandex.ru>
2019-12-07 21:48:25 +0600

    first commit - add main content

--- future_net_work/src/components/Header.jsx
+++ future_net_work/src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Header = () => {
     return <header className='header'>
-        <img src='https://avatars.mds.yandex.net/get-pdb/2128437/12d43701-d95d-4052-aaf1-f5ec0de1a23f/s1200' alt={}/>
+        <img src='https://avatars.mds.yandex.net/get-pdb/2128437/12d43701-d95d-4052-aaf1-f5ec0de1a23f/s1200'/>
     </header>
 };
 export default Header;
