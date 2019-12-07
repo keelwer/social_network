75920f1c56fcf3d2ed0637370980e36eec0b26dd
12 <keel-1@yandex.ru>
2019-12-07 21:47:05 +0600

    first commit - add main content

--- future_net_work/src/App.js
+++ future_net_work/src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import MainContent from "./components/MainContent";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='app-wrapper'>
+            <Header/>
+            <Navbar/>
+            <MainContent/>
+        </div>
+    );
 }
 
 export default App;
---
+++ future_net_work/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://avatars.mds.yandex.net/get-pdb/2128437/12d43701-d95d-4052-aaf1-f5ec0de1a23f/s1200' alt={}/>
+    </header>
+};
+export default Header;
