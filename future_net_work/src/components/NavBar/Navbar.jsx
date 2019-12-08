96e63c1d72201231d094f89802d1e8fa0697acea
12 <keel-1@yandex.ru>
2019-12-08 21:07:43 +0600

    Add color page

--- future_net_work/src/components/NavBar/Navbar.jsx
+++ future_net_work/src/components/NavBar/Navbar.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 import s from './Navbar.module.css'
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
-    return <nav className={s.nav}>
-        <div>
-            <a href="/profile">Profile</a>
-        </div>
-        <div>
-            <a href="/dialogs">Dialogs</a>
-        </div>
-        <div>
-            <a href="/news">News</a>
-        </div>
-        <div>
-            <a href="/music">Music</a>
-        </div>
-    </nav>
+    return (
+        <nav className={s.nav}>
+            <div className={s.item}>
+                <NavLink to="/profile" activeClassName={s.active}>Profile</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to="/dialogs" activeClassName={s.active}>Dialogs</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to="/news" activeClassName={s.active}>News</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to="/music" activeClassName={s.active}>Music</NavLink>
+            </div>
+        </nav>
+    )
 
 };
 export default Navbar;
